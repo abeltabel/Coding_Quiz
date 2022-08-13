b8dc1f78e304baf087f5e63b8bbc0fcfd056e168
Abeltabel <abel.fearnandez@gmail.com>
2022-08-13 15:56:23 -0600

    adding more Javascript making sure the questions are working

--- Main/assets/js/script.js
+++ Main/assets/js/script.js
@@ -18,3 +18,16 @@ class Question {
 }
 
 let questionList = [];
+
+const options1 = ["1. boolean", "2. object", "3. number", "4. string"];
+const question1 = new Question("What data types can local storage accept?", options1, "4. string");
+questionList.push(question1);
+
+const options2 = ["1. string", "2. number", "3. boolean", "4. all of the above"];
+const question2 = new Question("What data types can a function return?", options2, "4. all of the above");
+questionList.push(question2);
+
+const options3 = ["1. body", "2. canvas", "3. concept", "4. aside"];
+const question3 = new Question("What prameters can be passed into the query selector function?", options3, "2. css selectors");
+questionList.push(question3);
+
